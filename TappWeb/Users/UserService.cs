61c4cdc8351821b69269b24375397897756e4cc2
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-16 22:21:14 +0100

    Small change

--- TappWeb/Users/UserService.cs
+++ TappWeb/Users/UserService.cs
@@ -11,7 +11,7 @@ public interface IUserService
 
 public sealed class UserService : IUserService
 {
-    private IUserRepository _userRepository;
+    private readonly IUserRepository _userRepository;
 
     public UserService(IUserRepository userRepository)
     {
