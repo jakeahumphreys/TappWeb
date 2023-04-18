b109f0258d24727c9d023d2e08ff4f8136e0e486
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-18 15:56:24 +0100

    Spike out add user page

--- TappWeb/Pages/Users/Add.razor.cs
+++ TappWeb/Pages/Users/Add.razor.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Components;
+using TappWeb.Common.Helpers;
 using TappWeb.Services.Users;
 
 namespace TappWeb.Pages.Users;
@@ -8,28 +10,48 @@ public partial class Add
     [Inject] protected IUserService _userService { get; set; }
     
     public List<String> Errors { get; set; }
-    
-    public Guid Reference { get; set; }
-    public string Username { get; set; }
-    public string Firstname { get; set; }
-    public string Lastname { get; set; }
-    public string Email{ get; set; }
-    public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
-    public bool IsActive { get; set; }
 
+    public AddUserModel AddUserModel = new AddUserModel();
+    private bool success;
+    
     protected override void OnInitialized()
     {
         Errors = new List<string>();
-        Reference = Guid.NewGuid();
     }
 
-    public void AddUser()
+    public void OnValidSubmit()
     {
-        if (ConfirmPassword != Password)
-        {
-            Errors.Add("Passwords must match");
-            StateHasChanged();
-        }
+        var user = new UserBuilder()
+            .WithReference(Guid.NewGuid())
+            .WithUsername(AddUserModel.Username)
+            .WithFirstname(AddUserModel.Firstname)
+            .WithLastname(AddUserModel.Lastname)
+            .WithEmail(AddUserModel.Email)
+            .WithPassword(AddUserModel.Password)
+            .WithActiveStatus(AddUserModel.IsActive)
+            .CreateUser();
+
+        _userService.AddUser(user);
+        success = true;
+        StateHasChanged();
     }
+}
+
+public class AddUserModel
+{
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Firstname { get; set; }
+    [Required]
+    public string Lastname { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; }
+    [Required]
+    public bool IsActive { get; set; }
 }
